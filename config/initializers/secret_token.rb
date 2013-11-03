6d560d36484919873099f4aa7085ddcb035990ea
Alex Forey <me@alexforey.com>
2013-11-03 20:37:22 +0000

    Add all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LittleCommits::Application.config.secret_key_base = '3911fdd25fdf72c2674584eefbdf7ad5b617d47240fc90d7ab306c2927f2b6e4751a31166e22221556247223b5d3c385a65f62bc9284a87578be0ce6a1774e72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LittleCommits::Application.config.session_store :cookie_store, key: '_LittleCommits_session'
